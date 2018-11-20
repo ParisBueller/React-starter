328ab63d0d00beb64ddd2910b195aa6cf1c27342
Paris Ward <wardparis.pw@gmail.com>
2018-11-20 09:12:07 -0800

    completed

--- src/index.js
+++ src/index.js
@@ -8,10 +8,31 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatarImg={faker.image.avatar()} commentText="Great post!"/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatarImg={faker.image.avatar()} commentText="Well done, cheers"/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatarImg={faker.image.avatar()} commentText="Very informative"/>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    avatarImg={faker.image.avatar()} 
+                    commentText="Great post!"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00AM" 
+                    avatarImg={faker.image.avatar()} 
+                    commentText="Well done, cheers"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    avatarImg={faker.image.avatar()} 
+                    commentText="Very informative"
+                />
+            </ApprovalCard>
         </div>
     );
 };
