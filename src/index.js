0924cf2bae135b3a0858174263147f925d5fffb0
Paris Ward <wardparis.pw@gmail.com>
2018-11-19 17:55:43 -0800

    props and component nesting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-// Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-// Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div className="ui container comments">
+            <ApprovalCard />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatarImg={faker.image.avatar()} commentText="Great post!"/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatarImg={faker.image.avatar()} commentText="Well done, cheers"/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatarImg={faker.image.avatar()} commentText="Very informative"/>
+        </div>
+    );
 };
-
-// Take the react component and show it on the screen
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
